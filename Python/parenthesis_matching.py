508708988dcaa0f951958ca927a156f53418954b
JuanMartinElorriaga <54953433+JuanMartinElorriaga@users.noreply.github.com>
2019-09-06 13:22:41 -0300

    Update parenthesis_matching.py

--- Python/parenthesis_matching.py
+++ Python/parenthesis_matching.py
@@ -12,13 +12,15 @@ expresiones_cierre = [
 	"}"
 ] #Signos de cierre
 
-def balance_de_expresiones(entrada): #funcion basada en pila
+input_usuario = str(input("Colocar cadena a ser verificada: "))
+
+def balance_de_expresiones(input_usuario): #funcion basada en pila
 	pila = []
 	for i in entrada:
 		if i in expresiones_abertura:
 			pila.append(i)
 		elif i in expresiones_cierre:
-			position = expresiones_cierre.index(i) #busca elemento en closing expressions y devuelve el index
+			position = expresiones_cierre.index(i) #busca elemento en closing expresiones_cierre y devuelve el index
 			if ((len(pila) > 0) and 
 				(expresiones_abertura[position] == pila[len(pila)-1])):
 				pila.pop()
@@ -28,5 +30,5 @@ def balance_de_expresiones(entrada): #funcion basada en pila
 				print("BALANCEADO")
 
 
-balance_de_expresiones("][][][]))")
+balance_de_expresiones(input_usuario)
 
