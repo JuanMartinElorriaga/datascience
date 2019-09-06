cefdd58a80cb6993a941c7fb14cbcae83b5b72ea
JuanMartinElorriaga <54953433+JuanMartinElorriaga@users.noreply.github.com>
2019-09-06 13:10:04 -0300

    Update merger_excels_groupm.py

--- Python/merger_excels_groupm.py
+++ Python/merger_excels_groupm.py
@@ -1,31 +1,26 @@
 #CONSOLIDADOR DE ARCHIVOS EXCEL. Total de filas y columnas --- > estáticas
-#Colocar este archivo dentro de 
+#Colocar este archivo dentro de la carpeta contenedora de los archivos a consolidar (*.xls*)
 
 import pandas as pd
 from glob import glob
 from os import getcwd
+#######################################################################################################
 
-cwd = getcwd()
-excel_names = glob('H:\\Digital\\HUB\\Fronts\\Media_Planes\\2019\\Junio\\*.xls*') #Busca archivos Excel en la ruta especificada
- 
-excels = [file for file in directorio if file.endswith(".xlsm") or file.endswith(".xlsx")]
-
+#cwd = getcwd()
+excel_names = glob('H:\\Digital\\HUB\\Fronts\\Media_Planes\\2019\\Junio\\*.xls*') #Conjunto de archivos Excel en la ruta especificada
 df = pd.DataFrame()
 contador = 0
 
 for e in excel_names:
-	x = pd.read_excel(e, sheet_name='MEDIA PLAN')
-	#maximo = 10 + (x['Unnamed: 1'].count() - 6) + (x['Unnamed: 1'].isnull().sum() - 4) #HAY UN ERROR EN LA CUENTA DE LOS NULL!!!!!
-	#print(maximo)
-	#print(x['Unnamed: 1'].isnull().sum())
-	#print(x['Unnamed: 1'].count())
+	x = pd.read_excel(e, sheet_name = 'MEDIA PLAN')
 	x = x.loc[10:60,'Unnamed: 1':'Unnamed: 56']
 	df = df.append(x)
 	contador += 1
 	print(f"Copia de MP {contador} finalizada!")
 
-#print(df)
-#From DF to Excel:
-writer = pd.ExcelWriter('C:\\Users\\Juan.Elorriaga\\Desktop\\vieja pc\\Proyectos Py\\Bases_consolidadas\\EXCEL_UNIFICADO_JUNIO.xlsx')
+print(f"Cantidad total de MP: {contador}")
+
+#From df to Excel:
+writer = pd.ExcelWriter('C:\\Users\\Juan.Elorriaga\\Desktop\\vieja pc\\Proyectos Py\\Bases_consolidadas\\EXCEL_UNIFICADO.xlsx')
 df.to_excel(writer)
 writer.save()
